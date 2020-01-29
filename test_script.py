1c0f214604e0854794a55a242be80d12bc176403
Samuel Johnson <sdlj@uvic.ca>
2020-01-29 15:46:03 -0800

    testing commit. hope i dont break anything

---
+++ test_script.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import sys
+
+def main(*argv):
+    """
+
+    :param argv:
+    :return:
+    """
+    print("testing...")
+    for a in argv:
+        print(a)
+    print("...done.\n")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
